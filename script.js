e43e3ddb3a34e75ba982930f218590c9c42aec92
ViraVarguliak <varvera1486@gmail.com>
2025-03-19 10:57:47 +0100

    typing

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ var ids1 = ["ID001", "ID002", "ID003"]; // Только строки
 var ids2 = [101, 102, 103]; // Только числа
 // Массив с числами и строками одновременно
 var mixedValues = [1, "two", 3, "four", 5, "six"];
+// Либо строка, либо массив чисел
+var mixedValues1 = [1, 3, 5];
+// Либо массив чисел, либо массив строк
+var mixedValues2 = ["two", "four", "six"];
 // Массив булевых значений
 var toggles = [true, false, false, true, true];
 // Проверка значений (в TS тут должны появиться ошибки при несоответствии типов)
@@ -24,3 +28,40 @@ var toggles = [true, false, false, true, true];
 // inStock = "yes"; // Ошибка в TS
 // prices.push("Free"); // Ошибка в TS
 // products.push(100); // Ошибка в TS
+// Типизация Объекта
+var product = {
+    name: "Laptop",
+    price: 1499.99,
+    inStock: true,
+};
+var person = {
+    age: 38,
+    name: "Vra"
+};
+var product1 = {
+    name: "Laptop",
+    price: 1499.99,
+    inStock: true,
+};
+var direction = 'left';
+direction = 'right';
+direction = 'up';
+direction = 'right';
+direction = 'down';
+// direction = 'eight'; Ошибка типизаций значения переменой
+// Типизация функций
+var sum = function (a, b) {
+    return (
+    // Условие (тернарный оператор):
+    a + b > 100 ?
+        a + b :
+        a + b + ""
+    // условия ? если_правда : если_неправда
+    );
+};
+console.log(sum(200, 5));
+// В JavaScript (и TypeScript) оператор typeof используется для получения типа переменной или выражения. Он возвращает тип значения, которое вы проверяете, в виде строки.
+// function calculateDiscount(price: number, discount: number): number {
+//     return price - price * (discount / 100);
+//   }
+//   console.log(calculateDiscount(1000, 10)); // 900
--- script.ts
+++ script.ts
@@ -22,6 +22,10 @@ const ids2: number[] = [101, 102, 103]; // Только числа
 
 // Массив с числами и строками одновременно
 const mixedValues: (number | string)[] = [1, "two", 3, "four", 5, "six"];
+// Либо строка, либо массив чисел
+const mixedValues1: string | number[] = [1, 3, 5];
+// Либо массив чисел, либо массив строк
+const mixedValues2: string[] | number[] = ["two", "four", "six"];
 
 // Массив булевых значений
 const toggles: boolean[] = [true, false, false, true, true];
@@ -31,3 +35,67 @@ const toggles: boolean[] = [true, false, false, true, true];
 // inStock = "yes"; // Ошибка в TS
 // prices.push("Free"); // Ошибка в TS
 // products.push(100); // Ошибка в TS
+
+// Типизация Объекта
+const product: { 
+    name: string; 
+    price: number; 
+    inStock: boolean 
+} = {
+    name: "Laptop",
+    price: 1499.99,
+    inStock: true,
+  };
+
+interface IPerson {
+    name: string,
+    age: number
+}
+
+const person: IPerson = {
+    age:38,
+    name: "Vra"   
+}
+
+type Product = {
+    name: string;
+    price: number;
+    inStock: boolean;
+  };
+  
+  const product1: Product = {
+    name: "Laptop",
+    price: 1499.99,
+    inStock: true,
+  };
+type Direction = 'left' | 'right' | 'up' | 'down';
+
+let direction: 'left' | 'right' | 'up' | 'down' = 'left' ;
+direction = 'right';
+direction = 'up';
+direction = 'right';
+direction = 'down';
+
+// direction = 'eight'; Ошибка типизаций значения переменой
+
+// Типизация функций
+
+const sum = (a: number, b: number): string | number => {
+    return (
+        // Условие (тернарный оператор):
+        a + b > 100 ? 
+        a + b : 
+        a + b + ""
+        // условия ? если_правда : если_неправда
+    );
+};
+console.log(typeof sum(200, 5));
+// В JavaScript (и TypeScript) оператор typeof используется для получения типа переменной или выражения. Он возвращает тип значения, которое вы проверяете, в виде строки.
+
+
+// function calculateDiscount(price: number, discount: number): number {
+//     return price - price * (discount / 100);
+//   }
+  
+//   console.log(calculateDiscount(1000, 10)); // 900
+
